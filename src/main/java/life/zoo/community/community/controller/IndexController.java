bde735846c4f9cc2db5cbdba3b0e16ca6645d8da
Shirley <rjgclj@163.com>
2022-03-03 20:25:18 +0800

    add free login

--- src/main/java/life/zoo/community/community/controller/IndexController.java
+++ src/main/java/life/zoo/community/community/controller/IndexController.java
@@ -1,15 +1,36 @@
 package life.zoo.community.community.controller;
 
+import life.zoo.community.community.mapper.UserMapper;
+import life.zoo.community.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class IndexController {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index(){
+    public String index(HttpServletRequest request){
+        //免登录操作：获取cookie，若里面的token存在数据库
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")){
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if (user != null){
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
         return "index";
     }
 }
